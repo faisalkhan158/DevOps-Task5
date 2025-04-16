573b5bfeb44421ff5144837656be3989c147338e
faisalkhan158 <mdfaisalkhanofficial@gmail.com>
2025-04-16 17:17:19 +0530

    kubernetes deployment with minikube

---
+++ app/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from Node.js App running on Kubernetes!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
